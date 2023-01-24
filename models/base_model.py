83f40f503bcf99ed46a168c47a321552a94fa0cf
karanidenis <d.waweru@alustudent.com>
2023-01-24 21:40:18 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
 
     def __str__(self):
         """ readable format """
-        return f"[{__class__.__name__}]({self.id}){self.__dict__}"
+        return f"[{self.__class__.__name__}]({self.id}){self.__dict__}"
 
     def save(self):
         """
