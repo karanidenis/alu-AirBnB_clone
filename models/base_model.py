af8776b041a879ed16863542d06bc9175bdc8522
karanidenis <d.waweru@alustudent.com>
2023-01-24 21:37:11 +0300

    basemodel

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!usr/bin/python3
+
+"""
+the base model for the Airbnb clone project
+"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    The base class for all classes
+    """
+    def __init__(self):
+        """
+        initialising 
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ readable format """
+        return f"[{__class__.__name__}]({self.id}){self.__dict__}"
+
+    def save(self):
+        """
+        return a new value of updated_at
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """return dictionary"""
+        new_dict = self.__dict__.copy()
+        new_dict['self.created_at'] = self.created_at.isoformat()
+        new_dict['self.created_at'] = self.updated_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
