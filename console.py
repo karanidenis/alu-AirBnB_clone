dfd8478231896e6aa4b85db0a522a63f2446d100
karanidenis <d.waweru@alustudent.com>
2023-01-25 10:43:00 +0300

    basemodel

---
+++ console.py
@@ -0,0 +1,29 @@
+#!usr/bin/python3
+"""
+entry point of command interpreter
+"""
+
+import cmd
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """command interpreter"""
+    prompt = '(hbnb)'
+    classes = ['BaseModel']
+
+    def do_quit(self, line):
+        """ if quit is used exit"""
+        return True
+
+    def do_EOF(self, line):
+        """if EOF, exit"""
+        print()
+        return True
+
+    def empty_line(self):
+        """should do nothing"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
