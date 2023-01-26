a5223260c3402ca776417bce56a4fc741a96f97d
karanidenis <d.waweru@alustudent.com>
2023-01-26 16:50:11 +0300

    more classes

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" states module"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ define states class 
+    inherit from Basemodel """
+
+    name = ""
