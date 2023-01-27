36a1e50d16722813becefa38059f608b62265467
oluwabunmi2 <o.adebanjo@alustudent.com>
2023-01-27 06:31:59 -0800

    test

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""test Amenity"""
+import unittest
+import pep8
+import models
+import amenity
+from models.amenity import Amenity
+from models import storage
+
+class Amenity_testing(unittest.TestCase):
+    """Test Amenity class"""
+
+    def setUp(self):
+        """Return to "" class attributes"""
+        with open("test.json", 'w'):
+            storage._FileStorage__file_path = "test.json"
+            storage._FileStorage__objects = {}
+        Amenity.name = ""
+   
+    def testpep8(self):
+        """testing codestyle"""
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/amenity.py'
+        result = pepstylecode.check_files([path_user])
+        
+        def test_instance(self):
+         """test instance."""
+        amenity = Amenity()
+        self.assertIsInstance(amenity, Amenity)
+
+    def test_field_types(self):
+        """ Test field attributes of user """
+        my_Amenity = Amenity()
+        self.assertTrue(type(my_Amenity.name) == str)
+
+        if __name__ == '__main__':
+          unittest.main()
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Test case for review class"""
+import unittest
+import pep8
+from models import review
+from models.review import Place
+from models.base_model import BaseModel
+
+class TestReviewClass(unittest.TestCase):
+    """Test case for review class"""
+
+    def setUp(self):
+        """Class attribute"""
+        Review.place_id = ""
+        Review.user_id = ""
+        Review.text = ""
+
+    def test_pep8(self):
+        """ test base and test_base for pep8 conformance """
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/review.py'
+        file2 = 'tests/test_models/test_review.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")   
+
+    def test_is_instance(self):
+        """ Test if user is instance of basemodel """
+        my_Review = Review()
+        self.assertTrue(isinstance(my_Review, BaseModel))
+
+        def test_field_types(self):
+         """Test field"""
+        my_Review = Review()
+        self.assertTrue(type(my_Review.place_id) == str)
+        self.assertTrue(type(my_Review.user_id) == str)
+        self.assertTrue(type(my_Review.text) == str)
+
+
+if __name__ == '__main__':
+    unittest.main()
