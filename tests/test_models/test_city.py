d0d4f5983edb0a56fb2c4d03f04a944236440240
karanidenis <d.waweru@alustudent.com>
2023-01-30 15:52:07 +0300

    testcases

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -17,28 +17,16 @@ class City_testing(unittest.TestCase):
 
     
      def test_instance(self):
-         """test instance."""
-         city = City()
-         self.assertIsInstance(city, City)
-         # self.assertIsInstance(city, BaseModel)
-
-   #   def test_pep8(self):
-   #       """ test base and test_base for pep8 conformance """
-   #       style = pep8.StyleGuide(quiet=True)
-   #       file1 = 'models/city.py'
-   #       file2 = 'tests/test_models/test_city.py'
-   #       result = style.check_files([file1, file2])
-   #       self.assertEqual(result.total_errors, 0,
-   #                       "Found code style errors (and warning).")
+        """test instance."""
+        city = City()
+        self.assertIsInstance(city, City)
+        # self.assertIsInstance(city, BaseModel)
 
      def test_is_class(self):
          """test instance."""
          city = City()
          self.assertEqual(str(type(city)),
                         "<class 'models.city.City'>")
-
-     def test_amenity_is_a_subclass_of_basemodel(self):
-         self.assertTrue(issubclass(type(City), BaseModel))
 
      def test_field_types(self):
          """ Test field attributes of user """
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Test Place"""
 import unittest
-# import pep8
-# from models import place
 from models.place import Place
 from models.base_model import BaseModel
 
@@ -10,36 +8,25 @@ from models.base_model import BaseModel
 class TestPlaceClass(unittest.TestCase):
     """Test Place class"""
 
+    def setUp(self):
+        self.testplace = Place()
+
     def test_is_instance(self):
         """ Test if user is instance of basemodel """
-        my_place = Place()
-        self.assertTrue(isinstance(my_place, BaseModel))
-
-    # def test_pep8(self):
-    #     """test for pep8"""
-    #     style = pep8.StyleGuide(quiet=True)
-    #     file1 = 'models/place.py'
-    #     file2 = 'tests/test_models/test_place.py'
-    #     result = style.check_files([file1, file2])
-    #     self.assertEqual(result.total_errors, 0,
-    #                      "Found code style errors (and warning).")
-        
-    # def test_attrs_are_class_attrs(self):
-    #     for attr in self.attr_list:
-    #         self.assertTrue(hasattr(Place, attr))
+        self.assertTrue(isinstance(self.testplace, BaseModel))
 
     def test_class_attrs(self):
-        self.assertIs(type(self.my_place.name), str)
-        self.assertIs(type(self.my_place.city_id), str)
-        self.assertIs(type(self.my_place.user_id), str)
-        self.assertIs(type(self.my_place.description), str)
-        self.assertIs(type(self.my_place.number_bathrooms), int)
-        self.assertIs(type(self.my_place.max_guest), int)
-        self.assertIs(type(self.my_place.number_rooms), int)
-        self.assertIs(type(self.my_place.price_by_night), int)
-        self.assertIs(type(self.my_place.latitude), float)
-        self.assertIs(type(self.my_place.longitude), float)
-        self.assertIs(type(self.my_place.amenity_ids), list)
+        self.assertIs(type(self.testplace.name), str)
+        self.assertIs(type(self.testplace.city_id), str)
+        self.assertIs(type(self.testplace.user_id), str)
+        self.assertIs(type(self.testplace.description), str)
+        self.assertIs(type(self.testplace.number_bathrooms), int)
+        self.assertIs(type(self.testplace.max_guest), int)
+        self.assertIs(type(self.testplace.number_rooms), int)
+        self.assertIs(type(self.testplace.price_by_night), int)
+        self.assertIs(type(self.testplace.latitude), float)
+        self.assertIs(type(self.testplace.longitude), float)
+        self.assertIs(type(self.testplace.amenity_ids), list)
 
 
 if __name__ == '__main__':
