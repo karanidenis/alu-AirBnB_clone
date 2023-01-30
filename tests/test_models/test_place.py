63ec09f76c726a297c98acfd2da2d9f198075c09
karanidenis <d.waweru@alustudent.com>
2023-01-30 15:27:47 +0300

    testcases

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -24,22 +24,22 @@ class TestPlaceClass(unittest.TestCase):
     #     self.assertEqual(result.total_errors, 0,
     #                      "Found code style errors (and warning).")
         
-    def test_attrs_are_class_attrs(self):
-        for attr in self.attr_list:
-            self.assertTrue(hasattr(Place, attr))
+    # def test_attrs_are_class_attrs(self):
+    #     for attr in self.attr_list:
+    #         self.assertTrue(hasattr(Place, attr))
 
     def test_class_attrs(self):
-        self.assertIs(type(self.place.name), str)
-        self.assertIs(type(self.place.city_id), str)
-        self.assertIs(type(self.place.user_id), str)
-        self.assertIs(type(self.place.description), str)
-        self.assertIs(type(self.place.number_bathrooms), int)
-        self.assertIs(type(self.place.max_guest), int)
-        self.assertIs(type(self.place.number_rooms), int)
-        self.assertIs(type(self.place.price_by_night), int)
-        self.assertIs(type(self.place.latitude), float)
-        self.assertIs(type(self.place.longitude), float)
-        self.assertIs(type(self.place.amenity_ids), list)
+        self.assertIs(type(self.Place.name), str)
+        self.assertIs(type(self.Place.city_id), str)
+        self.assertIs(type(self.Place.user_id), str)
+        self.assertIs(type(self.Place.description), str)
+        self.assertIs(type(self.Place.number_bathrooms), int)
+        self.assertIs(type(self.Place.max_guest), int)
+        self.assertIs(type(self.Place.number_rooms), int)
+        self.assertIs(type(self.Place.price_by_night), int)
+        self.assertIs(type(self.Place.latitude), float)
+        self.assertIs(type(self.Place.longitude), float)
+        self.assertIs(type(self.Place.amenity_ids), list)
 
 
 if __name__ == '__main__':
